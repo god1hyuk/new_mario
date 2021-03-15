73d1b4653e34117ca5f5783c4f34d0fda2bfa4bf
wonhyukshin <wonhyuk6340@gmail.com>
2021-03-15 11:59:55 +0900

    v1.3
    20210315

--- js/new_mario.js
+++ js/new_mario.js
@@ -1,3 +1,4 @@
+// top button
 $(function(){
     $('.pipe').mouseover(function(){
         $('.standing_mario>img').stop().animate({
@@ -59,6 +60,7 @@ $(function(){
 });
 
 
+// main menu
 $(function(){
     $('.m_menu1>li:nth-child(1) img').mouseover(function(){
         $('.m_menu1>li:nth-child(1) img').attr('src','images/nes_glow.png').css('transform','scale(1.02)').css('transition','all 0.1s linear');
@@ -110,48 +112,80 @@ $(function(){
     });
 });
 
+
+
+
+
+// // ending animation
+// setTimeout(function () {
+//     $(".e_bm_wrap")
+//       .css("display", "block")
+//       .css("animation-play-state", "running");
+//   }, 1000);
+
+//   setTimeout(function () {
+//     $(".e_bm").animate(
+//       {
+//         animation: "none",
+//       },
+//       100
+//     ),
+//       $(".e_bm_run1").animate(
+//         {
+//           opacity: "0",
+//         },
+//         10
+//       ),
+//       $(".standing_bm").css("display", "block");
+//   }, 6000);
+
+//   setTimeout(function () {
+//     $(".castle_flag").animate(
+//       {
+//         top: "0",
+//       },
+//       1000
+//     );
+//   }, 7000);
+
+//   setTimeout(function () {
+//     $(".fireworks>img:nth-child(1)").css(
+//       "animation-play-state",
+//       "running"
+//     ),
+//       $(".fireworks>img:nth-child(2)").css(
+//         "animation-play-state",
+//         "running"
+//       ),
+//       $(".fireworks>img:nth-child(3)").css(
+//         "animation-play-state",
+//         "running"
+//       ),
+//       $(".fireworks>img:nth-child(4)").css(
+//         "animation-play-state",
+//         "running"
+//       );
+//   }, 8000);
+
+//   setTimeout(function () {
+//     $(".jumping_bm").css("display", "block"),
+//       $(".jumping_bm")
+//         .css("animation-play-state", "running")
+//         .css("display", "block"),
+//       $(".standing_bm").css("animation-play-state", "running"),
+//       $(".left_peach").css("animation-play-state", "running"),
+//       $(".front_peach").css("animation-play-state", "running");
+//   }, 10000);
+
+
+
+
+// scroll effect
 $(function(){
     $(window).scroll(function(){
-        if ($(this).scrollTop()>800) {
-            $('.sec2_title>span:nth-child(1)').delay(300).animate({
-                opacity: '1'
-            }, 10, "linear");
-            $('.sec2_title>span:nth-child(2)').delay(500).animate({
-                opacity: '1'
-            }, 10, "linear");
-            $('.sec2_title>span:nth-child(3)').delay(650).animate({
-                opacity: '1'
-            }, 10, "linear");
-            $('.sec2_title>span:nth-child(4)').delay(800).animate({
-                opacity: '1'
-            }, 10, "linear");
-            $('.sec2_title>span:nth-child(5)').delay(950).animate({
-                opacity: '1'
-            }, 10, "linear");
-            $('.sec2_title>span:nth-child(6)').delay(1050).animate({
-                opacity: '1'
-            }, 10, "linear");
-            $('.sec2_title>span:nth-child(7)').delay(1200).animate({
-                opacity: '1'
-            }, 10, "linear");
-            $('.mario_wrap').css('bottom','0').css('transition','all 0.5s ease'); 
-        }
-        else if($(this).scrollTop()<1200) {
-            $('.mario_wrap').css('bottom','-110px').css('transition','all 0.5s ease');
-        }
-        if ($(this).scrollTop()>300) {
-        //    $('.chasing_anime').animate({
-        //        opacity: '0'
-        //    }, 500);
-        }
-
-    });
-});
-
-
-
-
-
+        if ($(this).scrollTop()>7700) {
+            $('body').css("background","#192148").css("transition","all 1s ease-in-out");
+            // ending animation
 setTimeout(function () {
     $(".e_bm_wrap")
       .css("display", "block")
@@ -211,17 +245,41 @@ setTimeout(function () {
       $(".left_peach").css("animation-play-state", "running"),
       $(".front_peach").css("animation-play-state", "running");
   }, 10000);
-
-
-
-
-
-$(function(){
-    $(window).scroll(function(){
-        if ($(this).scrollTop()>5500) {
-            $('body').css("background","#192148").css("transition","all 1s ease-in-out");
         }
-        else if ($(this).scrollTop()>4400) {
+        else if ($(this).scrollTop()>6600) {
+            $('body').css("background","#e20019").css("transition","all 1s ease-in-out");
+            $('.sec8_title>span:nth-child(2)').delay(300).animate({
+                opacity: '1'
+            }, 10, "linear");
+            $('.sec8_title>span:nth-child(3)').delay(500).animate({
+                opacity: '1'
+            }, 10, "linear");
+            $('.sec8_title>span:nth-child(4)').delay(600).animate({
+                opacity: '1'
+            }, 10, "linear");
+            $('.sec8_title>span:nth-child(5)').delay(700).animate({
+                opacity: '1'
+            }, 10, "linear");
+            $('.sec8_title>span:nth-child(6)').delay(800).animate({
+                opacity: '1'
+            }, 10, "linear");
+            $('.sec8_title>span:nth-child(7)').delay(900).animate({
+                opacity: '1'
+            }, 10, "linear");
+        }
+        else if ($(this).scrollTop()>5500) {
+            $('body').css("background","#bb8743").css("transition","all 1s ease-in-out");
+            $('.sec7_title>span:nth-child(2)').delay(300).animate({
+                opacity: '1'
+            }, 10, "linear");
+            $('.sec7_title>span:nth-child(3)').delay(500).animate({
+                opacity: '1'
+            }, 10, "linear");
+            $('.sec7_title>span:nth-child(4)').delay(600).animate({
+                opacity: '1'
+            }, 10, "linear");
+        }
+        else if ($(this).scrollTop()>4500) {
             $('body').css("background","#71be59").css("transition","all 1s ease-in-out");
             $('.sec6_title>span:nth-child(2)').delay(300).animate({
                 opacity: '1'
@@ -233,7 +291,7 @@ $(function(){
                 opacity: '1'
             }, 10, "linear");
         }
-        else if ($(this).scrollTop()>3300) {
+        else if ($(this).scrollTop()>3500) {
             $('body').css("background","#b591de").css("transition","all 1s ease-in-out");
             $('.sec5_title>span:nth-child(2)').delay(300).animate({
                 opacity: '1'
@@ -257,22 +315,22 @@ $(function(){
                 opacity: '1'
             }, 10, "linear");
         }
-        else if ($(this).scrollTop()>2200) {
+        else if ($(this).scrollTop()>2800) {
             $('body').css("background","#fcb800").css("transition","all 1s ease-in-out");
             $('.sec4_title>span:nth-child(2)').delay(300).animate({
                 opacity: '1'
             }, 10, "linear");
-            $('.sec4_title>span:nth-child(3)').delay(500).animate({
+            $('.sec4_title>span:nth-child(3)').delay(400).animate({
                 opacity: '1'
             }, 10, "linear");
-            $('.sec4_title>span:nth-child(4)').delay(600).animate({
+            $('.sec4_title>span:nth-child(4)').delay(500).animate({
                 opacity: '1'
             }, 10, "linear");
-            $('.sec4_title>span:nth-child(5)').delay(700).animate({
+            $('.sec4_title>span:nth-child(5)').delay(600).animate({
                 opacity: '1'
             }, 10, "linear");
         }
-        else if($(this).scrollTop()>1200) {
+        else if($(this).scrollTop()>1500) {
             $('body').css("background","#1e3996").css("transition","all 1s ease-in-out");
             $('.sec3_title>span:nth-child(2)').delay(300).animate({
                 opacity: '1'
@@ -283,7 +341,32 @@ $(function(){
             $('.sec3_title>span:nth-child(4)').delay(600).animate({
                 opacity: '1'
             }, 10, "linear");
-        } 
+            $('.mario_wrap').css('bottom','-110px').css('transition','all 0.5s ease');
+        }
+        if ($(this).scrollTop()>800) {
+            $('.sec2_title>span:nth-child(1)').delay(300).animate({
+                opacity: '1'
+            }, 10, "linear");
+            $('.sec2_title>span:nth-child(2)').delay(400).animate({
+                opacity: '1'
+            }, 10, "linear");
+            $('.sec2_title>span:nth-child(3)').delay(500).animate({
+                opacity: '1'
+            }, 10, "linear");
+            $('.sec2_title>span:nth-child(4)').delay(600).animate({
+                opacity: '1'
+            }, 10, "linear");
+            $('.sec2_title>span:nth-child(5)').delay(700).animate({
+                opacity: '1'
+            }, 10, "linear");
+            $('.sec2_title>span:nth-child(6)').delay(800).animate({
+                opacity: '1'
+            }, 10, "linear");
+            $('.sec2_title>span:nth-child(7)').delay(900).animate({
+                opacity: '1'
+            }, 10, "linear");
+            $('.mario_wrap').css('bottom','0').css('transition','all 0.5s ease'); 
+        }
         else if ($(this).scrollTop()>400) {
             $('body').css("background","cadetblue").css("transition","all 1s ease-in-out");
         } 
@@ -294,7 +377,7 @@ $(function(){
 });
 
 
-
+// slide option
 $(function(){
     $('.bxslider').bxSlider({
       mode: 'fade',
